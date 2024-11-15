ba313fa829b84d925dc1faa34dd9803c0303eff8
ritesh goswami <riteshg@witmates.com>
2024-11-15 15:14:07 +0530

    enable production mode

--- apps/frontend/src/environment/environment.ts
+++ apps/frontend/src/environment/environment.ts
@@ -1,5 +1,5 @@
 const ENVIRONMENT = Object.freeze({
-   production: false,
+   production: true,
    get BASE_URL() {
       return this.production ? "https://chat-app-onh1.onrender.com" : "http://192.168.1.153:8000";
    },
